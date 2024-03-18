d95e74eedc61cd6952036c479e825c5d9ecfbdce
alvarodlo <alvarodelaorden@gmail.com>
2024-03-18 22:29:49 +0100

    import nutrition

--- src/main/java/ssdd/ArandaLeonGerardo_1/controllers/NutritionController.java
+++ src/main/java/ssdd/ArandaLeonGerardo_1/controllers/NutritionController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ssdd.ArandaLeonGerardo_1.entities.Nutrition;
 import ssdd.ArandaLeonGerardo_1.entities.User;
 import ssdd.ArandaLeonGerardo_1.service.NutritionService;
