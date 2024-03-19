1c3888fe0b233ed7872b7a005f8cd9ec1203d727
alvarodlo <alvarodelaorden@gmail.com>
2024-03-19 18:58:42 +0100

    corrections

--- src/main/java/ssdd/ArandaLeonGerardo_1/controllers/RoutineController.java
+++ src/main/java/ssdd/ArandaLeonGerardo_1/controllers/RoutineController.java
@@ -28,14 +28,19 @@ public class RoutineController {
         }
         return "redirect:/Portada";
     }
-    @GetMapping("/routines/{id}")
-    public String showRoutine(Model model, @PathVariable Long id){
-        Routine routine = routineService.getRoutine(id);
-        if(routine != null){
-            model.addAttribute("routine",routine);
-            return "showRoutine";
+    @GetMapping("/routines/{routineId}")
+    public String showRoutine(Model model, @PathVariable Long routineId, @RequestParam("id") Long id){
+        Routine routine = routineService.getRoutine(routineId);
+        if(routine == null){
+            return "redirect:/Portada";
         }
-        return "redirect:/Portada";
+        User user = userService.getUser(id);
+        if(user == null){
+            return "redirect:/Portada";
+        }
+        model.addAttribute("routine",routine);
+        model.addAttribute("userId",user.getId());
+        return "showRoutine";
     }
     @GetMapping("/routines/createRoutine")
     public String createRoutine(Model model, @RequestParam("id") Long id){
@@ -53,23 +58,31 @@ public class RoutineController {
         User user = userService.getUser(id);
         return "redirect:/routines?id=" + user.getId();
     }
-    @GetMapping("/routines/editRoutine/{id}")
-    public String editRoutine(Model model, @PathVariable Long id){
-        Routine routine = routineService.getRoutine(id);
-        if(routine != null){
-            model.addAttribute("routine",routine);
-            return "editRoutine";
+    @GetMapping("/routines/editRoutine/{routineId}")
+    public String editRoutine(Model model, @PathVariable Long routineId, @RequestParam("id") Long id){
+        Routine routine = routineService.getRoutine(routineId);
+        User user = userService.getUser(id);
+        if(user == null){
+            return "redirect:/Login";
         }
-        return "redirect:/Portada";
+        if(routine == null){
+            return "redirect:/frontPage?id=" + user.getId();
+        }
+        model.addAttribute("routine",routine);
+        return "editRoutine";
     }
-    @PostMapping("/routines/editRoutine/{id}")
-    public String editRoutinePost(Routine routine, @PathVariable Long id){
-        routineService.updateRoutine(id,routine);
+    @PostMapping("/routines/editRoutine/{routineId}")
+    public String editRoutinePost(Routine routine, @PathVariable Long routineId){
+        routineService.updateRoutine(routineId,routine);
         return "redirect:/routines";
     }
-    @PostMapping("/routines/delete/{id}")
-    public  String deleteRoutine(@PathVariable Long id){
-        routineService.deleteRoutine(id);
-        return "redirect:/routines";
+    @GetMapping("/routines/delete/{routineId}")
+    public  String deleteRoutinePost(@PathVariable Long routineId, @RequestParam("id") Long id){
+        routineService.deleteRoutine(routineId);
+        User user = userService.getUser(id);
+        if(user == null){
+            return "redirect:/Login";
+        }
+        return "redirect:/routines?id=" + user.getId();
     }
 }
