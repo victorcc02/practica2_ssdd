b734d880f1c6f3b6ed11f47dbca9079ca0d0a404
Gerardinhoatm <gerardrojiblanco@gmail.com>
2024-04-16 19:39:17 +0200

    Login controller

--- src/main/java/ssdd/practicaWeb/controllers/LoginController.java
+++ src/main/java/ssdd/practicaWeb/controllers/LoginController.java
@@ -6,26 +6,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import ssdd.practicaWeb.entities.User;
+import ssdd.practicaWeb.entities.GymUser;
 import ssdd.practicaWeb.service.UserService;
 
 @Controller
 public class LoginController {
     @Autowired
     private UserService userService;
+
+
     @GetMapping("/Login")
-    public String InterfazInicio(Model model){
-        model.addAttribute("user",new User());
+    public String InterfaceLogin(Model model){
+        model.addAttribute("user",new GymUser());
         return "login";
     }
     @PostMapping("/Login")
-    public String irPortada(User user) {
-        userService.createUser(user);
+    public String goFrontPage(GymUser user) {
+        userService.createGymUser(user);
         return "redirect:/FrontPage?id=" + user.getId();
     }
     @GetMapping("/FrontPage")
-    public String InterfazPortada(Model model, @RequestParam("id") Long id) {
-        model.addAttribute("user",userService.getUser(id));
+    public String InterfaceFrontPage(Model model, @RequestParam("id") Long id) {
+        model.addAttribute("user",userService.getGymUser(id));
         return "frontPage";
     }
 }
