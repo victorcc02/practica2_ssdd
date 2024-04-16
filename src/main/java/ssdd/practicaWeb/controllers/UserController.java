466df499cf25581f6e288dac709ab8c61f8434e3
Gerardinhoatm <gerardrojiblanco@gmail.com>
2024-04-16 19:42:36 +0200

    change gymuser in controller

--- src/main/java/ssdd/practicaWeb/controllers/UserController.java
+++ src/main/java/ssdd/practicaWeb/controllers/UserController.java
@@ -6,20 +6,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import ssdd.practicaWeb.entities.User;
+import ssdd.practicaWeb.entities.GymUser;
+import ssdd.practicaWeb.repositories.UserRepository;
 import ssdd.practicaWeb.service.UserService;
 
 @Controller
 public class UserController {
     /*Due to security reasons, it will not be possible to view the information of all
-    * existing users. A user will only be able to see details corresponding to
-    * their own account*/
+     * existing users. A user will only be able to see details corresponding to
+     * their own account*/
     @Autowired
     private UserService userService;
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/users/{id}")
     public String showProfile(Model model, @PathVariable Long id){
-        User user = userService.getUser((id));
+        GymUser user = userService.getGymUser((id));
         if(user != null){
             model.addAttribute("user",user);
             return "users";
@@ -29,7 +32,7 @@ public class UserController {
 
     @GetMapping("/users/edit/{id}")
     public String editeProfileGet(Model model,@PathVariable Long id){
-        User user = userService.getUser((id));
+        GymUser user = userService.getGymUser((id));
         if(user != null){
             model.addAttribute("user",user);
             return "edit";
@@ -38,14 +41,14 @@ public class UserController {
     }
 
     @PostMapping("/users/edit/{id}")
-    public String editProfilePost(User user, @PathVariable Long id){
-        userService.updateUser(id, user);
+    public String editProfilePost(GymUser user, @PathVariable Long id){
+        userService.updateGymUser(id, user);
         return "redirect:/users/{id}";
     }
 
     @GetMapping("/users/delete/{id}")
     public String deleteUser(@PathVariable Long id){
-        userService.deleteUser(id);
+        userService.deleteGymUser(id);
         return "redirect:/Inicio";
     }
 }
