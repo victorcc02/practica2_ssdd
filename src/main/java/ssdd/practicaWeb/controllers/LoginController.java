5aca8bef836a4890f801f28cf3d6735926789af6
victorcc02 <117302575+victorcc02@users.noreply.github.com>
2024-04-17 17:02:59 +0200

    check User

--- src/main/java/ssdd/practicaWeb/controllers/LoginController.java
+++ src/main/java/ssdd/practicaWeb/controllers/LoginController.java
@@ -7,14 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ssdd.practicaWeb.entities.GymUser;
+import ssdd.practicaWeb.repositories.UserRepository;
 import ssdd.practicaWeb.service.UserService;
+
+
+import java.util.Optional;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Controller
 public class LoginController {
     @Autowired
     private UserService userService;
 
-
+    @Autowired
+    private UserRepository userRepository;
     @GetMapping("/Login")
     public String InterfaceLogin(Model model){
         model.addAttribute("user",new GymUser());
@@ -22,12 +29,20 @@ public class LoginController {
     }
     @PostMapping("/Login")
     public String goFrontPage(GymUser user) {
-        userService.createGymUser(user);
-        return "redirect:/FrontPage?id=" + user.getId();
+        Optional<GymUser> theGymUser = userRepository.findById(user.getId());
+        if (theGymUser.isPresent()){
+            return "redirect:/FrontPage?id=" + user.getId();
+        }else {
+            userService.createGymUser(user);
+            return "redirect:/FrontPage?id=" + user.getId();
+        }
+
+
     }
     @GetMapping("/FrontPage")
     public String InterfaceFrontPage(Model model, @RequestParam("id") Long id) {
         model.addAttribute("user",userService.getGymUser(id));
         return "frontPage";
     }
+
 }
