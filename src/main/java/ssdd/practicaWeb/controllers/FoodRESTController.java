09a96df9c1d9cee14733a051c7a1d3013ee0f7a1
alvarodlo <alvarodelaorden@gmail.com>
2024-04-21 23:44:45 +0200

    create FoodRESTController

---
+++ src/main/java/ssdd/practicaWeb/controllers/FoodRESTController.java
@@ -0,0 +1,70 @@
+package ssdd.practicaWeb.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ssdd.practicaWeb.entities.Food;
+import ssdd.practicaWeb.service.FoodService;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/api/food")
+public class FoodRESTController {
+
+    @Autowired
+    private FoodService foodService;
+
+    @PostMapping
+    public ResponseEntity<Food> createFood(@RequestBody Food food) {
+        return ResponseEntity.status(201).body(foodService.createFood(food));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Food> getFood(@PathVariable Long id) {
+        Food food = foodService.getFood(id);
+        if (food == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(food);
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<Food>> allFoods() {
+        return ResponseEntity.ok(foodService.getAllFood());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Food> updateFood(@PathVariable Long id, @RequestBody Food food) {
+        Food update = foodService.updateFood(id, food);
+        if (update == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(update);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFood(@PathVariable Long id) {
+        foodService.deleteFood(id);
+        return ResponseEntity.ok().build();
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<Food> updateParcialFood(@PathVariable Long id, @RequestBody Food parcialFood) {
+        Food existed = foodService.getFood(id);
+        if (existed == null){
+            return ResponseEntity.notFound().build();
+        }
+        if (parcialFood.getName() != null) {
+            existed.setName(parcialFood.getName());
+        }
+        if (parcialFood.getType() != null) {
+            existed.setType(parcialFood.getType());
+        }
+        if (parcialFood.getCalories() != 0) {
+            existed.setCalories(parcialFood.getCalories());
+        }
+        foodService.updateFood(id, existed);
+        return ResponseEntity.ok(existed);
+    }
+}
