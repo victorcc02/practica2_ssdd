9abc0594adf4548a9ad2021cd6c2692e89e5cbaf
alvarodlo <alvarodelaorden@gmail.com>
2024-04-21 23:56:33 +0200

    repository changes

--- src/main/java/ssdd/practicaWeb/repositories/NutritionRepository.java
+++ src/main/java/ssdd/practicaWeb/repositories/NutritionRepository.java
@@ -1,7 +1,11 @@
 package ssdd.practicaWeb.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ssdd.practicaWeb.entities.GymUser;
 import ssdd.practicaWeb.entities.Nutrition;
 
+import java.util.List;
+
 public interface NutritionRepository extends JpaRepository<Nutrition, Long> {
+    List<Nutrition> findByGymUser(GymUser gymUser);
 }
--- src/main/java/ssdd/practicaWeb/repositories/RoutineRepository.java
+++ src/main/java/ssdd/practicaWeb/repositories/RoutineRepository.java
@@ -1,6 +1,11 @@
 package ssdd.practicaWeb.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ssdd.practicaWeb.entities.GymUser;
 import ssdd.practicaWeb.entities.Routine;
+
+import java.util.List;
+
 public interface RoutineRepository extends JpaRepository<Routine, Long> {
+    List<Routine> findByGymUser(GymUser gymUser);
 }
