ed7e8a888b9f9741f2166c20a1310146c37d46ec
Gerardinhoatm <gerardrojiblanco@gmail.com>
2024-04-21 23:41:08 +0200

    food controller

--- src/main/java/ssdd/practicaWeb/controllers/FoodController.java
+++ src/main/java/ssdd/practicaWeb/controllers/FoodController.java
@@ -9,23 +9,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ssdd.practicaWeb.entities.Food;
-import ssdd.practicaWeb.entities.GymUser;
-import ssdd.practicaWeb.entities.Nutrition;
 import ssdd.practicaWeb.repositories.FoodRepository;
 import ssdd.practicaWeb.service.FoodService;
-import ssdd.practicaWeb.service.NutritionService;
 import ssdd.practicaWeb.service.UserService;
+
 
 @Controller
 public class FoodController {
+
     @Autowired
     private FoodService foodService;
     @Autowired
     private FoodRepository foodRepository;
     @Autowired
     private UserService userService;
-    @Autowired
-    private NutritionService nutritionService;
 
     @PostConstruct
     public void foodConstructor(){
@@ -62,75 +59,47 @@ public class FoodController {
         foodService.createFood(costillar);
     }
 
-
-    @GetMapping("/ListFoods")
-    public String InterfaceListFood(@RequestParam Long id, Model model) {
-        model.addAttribute("food", foodService.getAllFood());
-        Nutrition nutrition = nutritionService.getNutrition(id);
-        if (nutrition != null) {
-            model.addAttribute("nutrition", nutrition);
-            model.addAttribute("nutritionId", nutrition.getId());
-            return "foodSearch";
-        }
-        return "redirect:/ListNutrition";
-    }
-
     @GetMapping("/ListFoods/CreateFood")
-    public String InterfaceCreateNutrition(@RequestParam Long id, Model model) {
+    public String InterfaceCreateFood(@RequestParam Long id, Model model) {
         model.addAttribute("food",new Food());
-        GymUser user = userService.getGymUser(id);
-        if (user != null){
-            model.addAttribute("userId",user.getId());
-            return "createFood";
-        }
-        return "redirect:/ListNutrition";
+        model.addAttribute("nutritionId", id);
+        return "createFood";
     }
     @PostMapping("/ListFoods/CreateFood")
     public String addFood(Food food, @RequestParam("id") Long id){
-        GymUser user = userService.getGymUser(id);
-        if (user != null){
-            foodService.createFood(food);
-            return "redirect:/ListFoods?id=" + user.getId();
-        }
-        return "redirect:/FrontPage";
-
+        foodService.createFood(food);
+        return "redirect:/ListFoods?id=" + id;
     }
+
     @GetMapping("/ListFoods/detailsFood/{id}")
-    public String detailsFood(@PathVariable Long id, Model model){
+    public String detailsFood(@PathVariable Long id, Model model, @RequestParam("id") Long nutritionId){
         Food food = foodService.getFood(id);
         if (food == null) {
             return "redirect:/ListFoods";
         }
         model.addAttribute("food", food);
-        if(food != null){
-            return "detailsFood";
-        }
-        return "redirect:/ListFoods";
+        model.addAttribute("nutritionId", nutritionId);
+        return "detailsFood";
     }
 
     @GetMapping("/ListFoods/ModifyFood/{foodId}")
-    public String showFormEdit(@PathVariable Long foodId, Model model, @RequestParam("id") Long userId) {
+    public String showFormEdit(@PathVariable Long foodId, Model model, @RequestParam("id") Long nutritionId) {
         Food food = foodService.getFood(foodId);
         if(food != null){
             model.addAttribute("food", food);
         }
-        GymUser user = userService.getGymUser(userId);
-        if(user != null){
-            model.addAttribute("userId",user.getId());
-            return "modifyFood";
-        }
-        return "redirect:/ListFoods";
+        model.addAttribute("nutritionId",nutritionId);
+        return "modifyFood";
     }
     @PostMapping("/ListFoods/ModifyFood/{foodId}")
     public String editFood(Food food,@PathVariable Long foodId,@RequestParam("id") Long id) {
         foodService.updateFood(foodId, food);
-        GymUser user = userService.getGymUser(id);
-        return "redirect:/ListFoods?id=" + user.getId();
+        return "redirect:/ListFoods?id=" + id;
     }
     @GetMapping("/ListFoods/DeleteFood/{id}")
-    public String deleteFood(@PathVariable Long id, @RequestParam("id") Long userId) {
+    public String deleteFood(@PathVariable Long id, @RequestParam("id") Long nutritionId) {
         foodService.deleteFood(id);
-        GymUser user = userService.getGymUser(userId);
-        return "redirect:/ListFoods?id=" + user.getId();
+        return "redirect:/ListFoods?id=" + nutritionId;
     }
+
 }
